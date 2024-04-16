628af9f7c174625720ceab6bec0b0dd9f2b3bb04
dguimaraes <dguimaraes@gmail.com>
2024-04-15 22:40:23 -0300

    :sparkles: Adiciona component Footer

---
+++ src/Components/Footer/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="bg-gray-900 text-white py-4 text-center w-full fixed bottom-0">
+            <div className="container mx-auto">
+                Desenvolvido por Daniel Guimarães
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
