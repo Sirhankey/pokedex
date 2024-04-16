595b38fef398c1d350117d7a9444e7499ea80890
dguimaraes <dguimaraes@gmail.com>
2024-04-15 22:41:21 -0300

    :sparkles: Adiciona pages e components ao routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Pages/Login/App';
 import Home from './Pages/Home/App';
 import { PokemonsProvider } from './Contextos/Pokemons';
+import Detail from './Pages/Detail/App';
+import Header from './Components/Header/App';
+import Footer from './Components/Footer/App';
 
 function AppRoutes() {
     const [loggedIn, setLoggedIn] = useState(true);
@@ -18,6 +21,7 @@ function AppRoutes() {
             {/* <UserProvider> */}
             <PokemonsProvider>
                 {/* <PokedexProvider> */}
+                <Header isLoggedIn={loggedIn} />
                 <Routes>
                     <Route
                         path="/"
@@ -27,11 +31,16 @@ function AppRoutes() {
                         path="/home"
                         element={!loggedIn ? <Navigate to="/" /> : <Home />}
                     />
+                    <Route
+                        path="/detail/:id"
+                        element={!loggedIn ? <Navigate to="/" /> : <Detail />}
+                    />
                     {/* <Route path="/detail/:pokemonId" component={DetailPage} /> */}
                     {/* <Route path="/cadastro" component={CadastroPage} /> */}
                     {/* <Route path="/pokedex" component={PokedexPage} /> */}
                     {/* <Route path="/quiz" component={QuizPage} /> */}
                 </Routes>
+                <Footer />
                 {/* </PokedexProvider> */}
             </PokemonsProvider>
             {/* </UserProvider> */}
