07faf5cbcea658e95411c75b6bb5d1573c8c38a5
dguimaraes <dguimaraes@gmail.com>
2024-04-16 16:59:51 -0300

    :construction: Altera page Login

--- src/Pages/Login/App.js
+++ src/Pages/Login/App.js
@@ -1,28 +1,43 @@
 import React, { useState } from 'react';
+import { logarUsuario } from '../../Services/usuario';
 
 const Login = ({ onLogin }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [usuario, setUsuario] = useState({ id: null, email: '', password: '' });
+
     const [isFormValid, setIsFormValid] = useState(false);
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
+    const handleChange = (event) => {
+        const { value, name } = event.target;
+        // console.log(event);
+        setUsuario((objetoAtual) => ({
+            ...objetoAtual,
+            [name]: value
+        }));
         validateForm();
-    };
-
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-        validateForm();
-    };
+    }
 
     const validateForm = () => {
-        setIsFormValid(email !== '' && password !== '' && password.length >= 6 && /\d/.test(password) && /[a-zA-Z]/.test(password));
+        setIsFormValid(
+            usuario.email !== '' &&
+            usuario.password !== '' &&
+            usuario.password.length >= 6 &&
+            /\d/.test(usuario.password) &&
+            /[a-zA-Z]/.test(usuario.password));
     };
 
-    const handleLogin = () => {
-        if (isFormValid) {
-            onLogin();
-        }
+    const handleLogin = (event) => {
+        console.log('event', event);
+        event.preventDefault();
+        console.log('usuario', usuario);
+        logarUsuario(usuario).then((usuario) => {
+            console.log('usuario!!!', usuario);
+            if (usuario) {
+                onLogin(usuario);
+            }
+        }).catch((error) => {
+            console.error('Erro ao logar', error);
+            alert('Erro ao logar');
+        });
     };
 
     return (
@@ -31,13 +46,28 @@ const Login = ({ onLogin }) => {
                 <h2 className="text-2xl font-bold mb-4">Login</h2>
                 <div className="mb-4">
                     <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email</label>
-                    <input type="email" id="email" className="w-full border border-gray-300 rounded px-3 py-2" value={email} onChange={handleEmailChange} />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="email@email.com"
+                        className="w-full border border-gray-300 rounded px-3 py-2"
+                        onChange={handleChange} />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
-                    <input type="password" id="password" className="w-full border border-gray-300 rounded px-3 py-2" value={password} onChange={handlePasswordChange} />
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Senha"
+                        className="w-full border border-gray-300 rounded px-3 py-2"
+                        onChange={handleChange} />
                 </div>
-                <button className={`w-full bg-red-500 text-white font-bold py-2 px-4 rounded ${isFormValid ? 'cursor-pointer' : 'cursor-not-allowed'}`} onClick={handleLogin}>
+                <button
+                    type="submit"
+                    className={`w-full bg-red-500 text-white font-bold py-2 px-4 rounded ${isFormValid ? 'cursor-pointer' : 'cursor-not-allowed'}`}
+                    onClick={handleLogin}>
                     <span className="animate-pokeball"></span>
                     Entrar
                 </button>
