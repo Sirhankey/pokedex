8922fd8515fce53e39c78cfdd54deacc5b082a8f
dguimaraes <dguimaraes@gmail.com>
2024-04-16 23:51:49 -0300

    :construction: Adiciona config e dependencia

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
     extend: {
       colors: {
         purple: '#AD61AE',
+        gold: '#FFD700',
       },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }
     },
   },
   plugins: [],
