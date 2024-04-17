68b54043d568c3b51b261ed148c6461ee4b19d7b
dguimaraes <dguimaraes@gmail.com>
2024-04-16 23:52:43 -0300

    :feat: Cria component Carousel

---
+++ src/Components/Carousel/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function CustomCarousel({ revealedPokemons }) {
+    if (Object.keys(revealedPokemons).length < 10) return null;
+
+    return (
+        <div
+            x-data="{}"
+            x-init="$nextTick(() => {
+                let ul = $refs.logos;
+                ul.insertAdjacentHTML('afterend', ul.outerHTML);
+                ul.nextSibling.setAttribute('aria-hidden', 'true');
+            })"
+            className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
+        >
+            <ul x-ref="logos" className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+                {Object.keys(revealedPokemons).map((key) => (
+                    <li key={key}>
+                        <img src={revealedPokemons[key].sprite} alt={revealedPokemons[key].name} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default CustomCarousel;
