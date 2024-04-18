912c3ac593a17a9ee7cc1611bb40cd874b6b0618
dguimaraes <dguimaraes@gmail.com>
2024-04-18 01:28:03 -0300

    :sparkles: Adiciona function

--- src/Utils/util.js
+++ src/Utils/util.js
@@ -63,3 +63,10 @@ export function getTypeColor(type) {
     }
 }
 
+export function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
