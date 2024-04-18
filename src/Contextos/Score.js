dc069a2602d4db044287a49bf608182e37a6c089
dguimaraes <dguimaraes@gmail.com>
2024-04-18 04:29:38 -0300

    :feat: Cria context score

---
+++ src/Contextos/Score.js
@@ -0,0 +1,60 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { auth, db } from "../Infra/firebase";
+import { get, ref, set } from "firebase/database";
+import { uid } from "uid";
+import { onAuthStateChanged } from "firebase/auth"; // Importar onAuthStateChanged
+
+export const ScoreContext = createContext();
+
+export function ScoreProvider({ children }) {
+    const [scores, setScores] = useState([]);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                console.log('Usuário logado detectado, carregando scores...');
+                fetchUserScores(user.uid);
+            } else {
+                console.log('Nenhum usuário logado.');
+                setScores([]);
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    const fetchUserScores = async (userId) => {
+        const scoresRef = ref(db, `scores/${userId}`);
+        const snapshot = await get(scoresRef);
+        if (snapshot.exists()) {
+            setScores(Object.values(snapshot.val()));
+        } else {
+            setScores([]);
+        }
+    };
+
+    const postScore = async (newScore) => {
+        if (auth.currentUser) {
+            const newUid = uid();
+            const timeStamp = new Date().getTime();
+            const email = auth.currentUser.email;
+
+            await set(ref(db, `scores/${auth.currentUser.uid}/${newUid}`), {
+                score: newScore,
+                uid: newUid,
+                timeStamp: timeStamp,
+                email: email
+            });
+        }
+    };
+
+    return (
+        <ScoreContext.Provider value={{ scores, postScore }}>
+            {children}
+        </ScoreContext.Provider>
+    );
+}
+
+export function useScoreContext() {
+    return useContext(ScoreContext);
+}
