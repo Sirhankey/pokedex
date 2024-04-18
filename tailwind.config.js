31b71ac169f3bf87a39782200132d9ffac79b78c
dguimaraes <dguimaraes@gmail.com>
2024-04-18 01:24:23 -0300

    :feat: Adiciona configs tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,22 @@ module.exports = {
       colors: {
         purple: '#AD61AE',
         gold: '#FFD700',
+        silver: '#C0C0C0',
+        bronze: '#CD7F32',
       },
       animation: {
         'infinite-scroll': 'infinite-scroll 25s linear infinite',
+        spin: 'spin 1s linear infinite',
       },
       keyframes: {
         'infinite-scroll': {
           from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
-        }
+        },
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
       }
     },
   },
