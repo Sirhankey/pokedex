8234bf54777d89cc51381623e08bd6cce9d61bb6
dguimaraes <dguimaraes@gmail.com>
2024-04-18 01:25:43 -0300

    :construction: Ajusta component

--- src/Components/Carousel/App.js
+++ src/Components/Carousel/App.js
@@ -13,7 +13,7 @@ function CustomCarousel({ revealedPokemons }) {
             })"
             className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
         >
-            <ul x-ref="logos" className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+            <ul x-ref="logos" className="flex w-full items-center justify-center md:justify-center [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
                 {Object.keys(revealedPokemons).map((key) => (
                     <li key={key}>
                         <img src={revealedPokemons[key].sprite} alt={revealedPokemons[key].name} />
